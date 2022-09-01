d523423b9bbf552a5f2c0d74347e5d181c525cd1
Pavan Satish <pavan19082001@gmail.com>
2022-09-01 11:01:48 +0530

    first commit

---
+++ src/main/java/com/bootcamp/funds/OnlineFundRaisingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.funds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFundRaisingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFundRaisingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/funds/OnlineFundRaisingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.funds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineFundRaisingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
